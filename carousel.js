391c93cb03300ca3e43a436c181d6b3d39113ce1
Renaldo Petlim <renaldopetlim@gmail.com>
2024-08-18 11:29:54 -0300

    Ajuste 4.0

--- carousel.js
+++ carousel.js
@@ -5,7 +5,7 @@ const nextButton = document.querySelector('.next');
 
 let index = 0; // Mantém o controle de qual imagem está sendo exibida.
 
-function updateCarousel() {
+function updateCarousel() { // Usado para atualizar a posição do carrossel na tela.
     const width = images[0].clientWidth;
     carousel.style.transform = `translateX(${-index * width}px)`; // Move o carrossel horizontalmente.
 }
@@ -19,5 +19,6 @@ prevButton.addEventListener('click', () => {
     index = (index - 1 + images.length) % images.length;
     updateCarousel();
 }); // Adiciona a funcionalidade "PREV" ao botão de navegação.
+
 
 window.addEventListener('resize', updateCarousel);
